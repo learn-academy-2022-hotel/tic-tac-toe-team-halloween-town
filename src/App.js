ae78e486393ffda7ec63985a08a97b11ffe11f16
RaymondOchoa <ochoaraymondb@yahoo.com>
2023-01-03 16:25:59 -0800

    As a user, when either my partner or I win the game (three squares in a row: horizontally, vertically, or diagonally) I can see a notice telling me which player won.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,16 @@ import './App.css'
 const App = () => {
   const [squares, setSquares] = useState(Array(9).fill(null))
   const [counter, setCounter] = useState(0)
+  const winner = calculateWinner(squares)
+  const [xIsNext, setXisNext] = useState(true)
+  
 
-  const handleGamePlay = (clickedSquare) => {
+  const handleGamePlay = (clickedSquare, index) => {
+    let updateBoard = [...squares]
+    if (winner || updateBoard[index]) 
+    return updateBoard[index] = xIsNext ? "X":"O"
+    setSquares(updateBoard)
+    setXisNext(!xIsNext)
     if(squares[clickedSquare] !== null){
       return
     }
@@ -22,7 +30,28 @@ const App = () => {
     let newCounter = counter + 1
     setCounter(newCounter)
   } 
-
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+      else if (squares.includes(lines)){
+        return alert("You Won")
+      }
+    }
+    return null
+    }
   return (
     <>
       <h1>Tic Tac Toe</h1>
@@ -33,7 +62,9 @@ const App = () => {
             square={square}
             index={index}
             handleGamePlay={handleGamePlay}
-            key={index}
+            calculateWinner={calculateWinner}
+            winner={winner}
+           
             />
         )
       })}
